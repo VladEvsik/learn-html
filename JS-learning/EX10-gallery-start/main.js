8888594f4f8beaf18521aef533fa5dfbf6116aaf
mol <102357113+VladEvsik@users.noreply.github.com>
2022-08-07 10:53:32 +0500

    2nd try

--- JS-learning/EX10-gallery-start/main.js
+++ JS-learning/EX10-gallery-start/main.js
@@ -5,7 +5,7 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-  var nameImg = pic;
+  var nameImg = 'pic';
 for (var i = 1; i <= 5; i++){
   nameImg = nameImg + i + '.jpg'
   var newImage = document.createElement('img');
