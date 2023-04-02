8f4a684670aefd29188dc7f6fd30837cf66c7ab7
VladEvsik <vladislav.evsik@gmail.com>
2023-04-02 09:05:10 +0500

    1st example of callbacks

--- JS-learning/EX19-sequencing-animations/main.js
+++ JS-learning/EX19-sequencing-animations/main.js
@@ -13,4 +13,16 @@ const aliceTumbling = [
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
 
-  alice1.animate(aliceTumbling, aliceTiming);
+  function doSteps() {
+    alice1.animate(aliceTumbling, aliceTiming).finished.then(() => {
+      alice2.animate(aliceTumbling, aliceTiming).finished.then(() => {
+        alice3.animate(aliceTumbling, aliceTiming)
+      });
+    });
+    
+  }
+
+  doSteps();
+
+  
+  
